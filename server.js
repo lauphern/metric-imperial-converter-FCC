fffff01fcaf2daeffe7008374ecc5a174bf34298
lauphern <lau.p.hern@hotmail.com>
2020-08-10 21:41:54 +0200

    setup CSP from Helmet to prevent cross-site scripting (XSS) attacks

--- server.js
+++ server.js
@@ -1,58 +1,68 @@
-'use strict';
-if(process.env.NODE_ENV !== 'production') require('dotenv').config();
+"use strict";
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expect = require("chai").expect;
+const cors = require("cors");
 const helmet = require("helmet");
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
 const app = express();
 
 app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "default-src": ["'self'"],
+      "script-src": ["'self'", 'https://code.jquery.com', "'unsafe-inline'"],
+      "connect-src": ["'self'"],
+      "img-src": ["'self'"],
+      "style-src": ["'self'", "'unsafe-inline'"],
+      "base-uri": ["'self'"],
+      "form-action": ["'self'"],
+    },
+  })
+);
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
 });
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         let error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+        console.log("Tests are not valid:");
+        console.log(error);
       }
     }, 1500);
   }
