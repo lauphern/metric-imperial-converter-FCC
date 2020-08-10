5893d4b99754e0a14473544f1e8abda1560b6407
lauphern <lau.p.hern@hotmail.com>
2020-08-10 20:06:40 +0200

    finished the fourth suite of unit tests: spellOutUnit

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -89,7 +89,11 @@ suite('Unit Tests', function(){
   suite('Function convertHandler.spellOutUnit(unit)', function() {
     
     test('For Each Valid Unit Inputs', function(done) {
-      //see above example for hint
+      const input = ['gal','l','mi','km','lbs','kg'];
+      const expect = ['gallons','litres','miles','kilometres','pounds','kilograms'];
+      input.forEach(function(el, i) {
+        assert.equal(convertHandler.spellOutUnit(el), expect[i]);
+      });
       done();
     });
     
