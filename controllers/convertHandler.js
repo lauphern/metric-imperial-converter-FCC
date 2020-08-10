ab842ce76e44f800af84284da7a809382eed06de
lauphern <lau.p.hern@hotmail.com>
2020-08-10 13:00:23 +0200

    convert method done in ConvertHandler

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -9,25 +9,25 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    var result;
+    let result;
     
     return result;
   };
   
   this.getUnit = function(input) {
-    var result;
+    let result;
     
     return result;
   };
   
   this.getReturnUnit = function(initUnit) {
-    var result;
+    let result;
     
     return result;
   };
 
   this.spellOutUnit = function(unit) {
-    var result;
+    let result;
     
     return result;
   };
@@ -36,13 +36,32 @@ function ConvertHandler() {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
-    
+    let result;
+    switch(initUnit) {
+      case "gal":
+        result = initNum * galToL;
+        break;
+      case "L":
+        result = initNum / galToL;
+        break;
+      case "lbs":
+        result = initNum * lbsToKg;
+        break;
+      case "kg":
+        result = initNum / lbsToKg;
+        break;
+      case "mi":
+        result = initNum * miToKm;
+        break;
+      case "km":
+        result = initNum / miToKm;
+        break;
+    }
     return result;
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
+    let result;
     
     return result;
   };
