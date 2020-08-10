eb1318b32f6409b025f33d91838d822ff4382134
lauphern <lau.p.hern@hotmail.com>
2020-08-10 18:52:02 +0200

    added status code to responses

--- routes/api.js
+++ routes/api.js
@@ -23,8 +23,18 @@ module.exports = function (app) {
       const returnNum = convertHandler.convert(initNum, initUnit);
       const returnUnit = convertHandler.getReturnUnit(initUnit);
       const toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+      const body = {initNum, initUnit, returnNum, returnUnit, string: toString}
       
-      res.json({initNum, initUnit, returnNum, returnUnit, string: toString});
+      if(toString === "invalid number and unit") {
+        return res.status(400).json(body);
+      } else if(initNum === "invalid number" || returnNum === "invalid number" || toString === "invalid number") {
+        return res.status(400).json(body);
+      } else if(initUnit === "invalid unit" || returnUnit === "invalid unit" || returnNum === "invalid unit" || toString === "invalid unit") {
+        return res.status(400).json(body);
+      }
+
+      res.status(200).json(body);
     });
     
 };
